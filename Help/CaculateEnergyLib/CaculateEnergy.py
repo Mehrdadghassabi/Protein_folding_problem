9f15f0313c7fe8d928346289b1b7a543826913d1
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2024-01-24 00:09:24 +0330

    Delete Help directory

--- Help/CaculateEnergyLib/CaculateEnergy.py
+++
@@ -1,9 +0,0 @@
-from protein_class import Protein
-from dict_output import make_dictionary
-from dataframefunction import check_if_crd, final_comparison_df
-import ecalc as e
-import os
-from timeit import default_timer as timer
-import numpy as np
-import json
-import csv
