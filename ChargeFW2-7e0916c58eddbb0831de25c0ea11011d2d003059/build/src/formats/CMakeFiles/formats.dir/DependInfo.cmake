05412c51e76291b435ded4fef7d7092af4f810c3
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2024-01-23 18:44:38 +0330

    Add files via upload

---
+++ ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/formats/CMakeFiles/formats.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/bonds.cpp" "src/formats/CMakeFiles/formats.dir/bonds.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/bonds.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/cif.cpp" "src/formats/CMakeFiles/formats.dir/cif.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/cif.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/common.cpp" "src/formats/CMakeFiles/formats.dir/common.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/common.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/mmcif.cpp" "src/formats/CMakeFiles/formats.dir/mmcif.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/mmcif.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/mol2.cpp" "src/formats/CMakeFiles/formats.dir/mol2.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/mol2.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/pdb.cpp" "src/formats/CMakeFiles/formats.dir/pdb.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/pdb.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/pqr.cpp" "src/formats/CMakeFiles/formats.dir/pqr.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/pqr.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/reader.cpp" "src/formats/CMakeFiles/formats.dir/reader.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/reader.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/sdf.cpp" "src/formats/CMakeFiles/formats.dir/sdf.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/sdf.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/txt.cpp" "src/formats/CMakeFiles/formats.dir/txt.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/txt.cpp.o.d"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/formats/writer.cpp" "src/formats/CMakeFiles/formats.dir/writer.cpp.o" "gcc" "src/formats/CMakeFiles/formats.dir/writer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/structures/CMakeFiles/structures.dir/DependInfo.cmake"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/CMakeFiles/geometry.dir/DependInfo.cmake"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/utility/CMakeFiles/utility.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/formats/CMakeFiles/formats.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formats.dir/bonds.cpp.o"
+  "CMakeFiles/formats.dir/bonds.cpp.o.d"
+  "CMakeFiles/formats.dir/cif.cpp.o"
+  "CMakeFiles/formats.dir/cif.cpp.o.d"
+  "CMakeFiles/formats.dir/common.cpp.o"
+  "CMakeFiles/formats.dir/common.cpp.o.d"
+  "CMakeFiles/formats.dir/mmcif.cpp.o"
+  "CMakeFiles/formats.dir/mmcif.cpp.o.d"
+  "CMakeFiles/formats.dir/mol2.cpp.o"
+  "CMakeFiles/formats.dir/mol2.cpp.o.d"
+  "CMakeFiles/formats.dir/pdb.cpp.o"
+  "CMakeFiles/formats.dir/pdb.cpp.o.d"
+  "CMakeFiles/formats.dir/pqr.cpp.o"
+  "CMakeFiles/formats.dir/pqr.cpp.o.d"
+  "CMakeFiles/formats.dir/reader.cpp.o"
+  "CMakeFiles/formats.dir/reader.cpp.o.d"
+  "CMakeFiles/formats.dir/sdf.cpp.o"
+  "CMakeFiles/formats.dir/sdf.cpp.o.d"
+  "CMakeFiles/formats.dir/txt.cpp.o"
+  "CMakeFiles/formats.dir/txt.cpp.o.d"
+  "CMakeFiles/formats.dir/writer.cpp.o"
+  "CMakeFiles/formats.dir/writer.cpp.o.d"
+  "libformats.a"
+  "libformats.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formats.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
