d3b7e57156ec7d5d0379f2d82aaa833456b493b6
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2024-01-23 18:47:19 +0330

    Add files via upload

---
+++ ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/CMakeFiles/libchargefw2.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libchargefw2.dir/element.cpp.o"
+  "CMakeFiles/libchargefw2.dir/element.cpp.o.d"
+  "CMakeFiles/libchargefw2.dir/geometry.cpp.o"
+  "CMakeFiles/libchargefw2.dir/geometry.cpp.o.d"
+  "CMakeFiles/libchargefw2.dir/method.cpp.o"
+  "CMakeFiles/libchargefw2.dir/method.cpp.o.d"
+  "CMakeFiles/libchargefw2.dir/parameters.cpp.o"
+  "CMakeFiles/libchargefw2.dir/parameters.cpp.o.d"
+  "CMakeFiles/libchargefw2.dir/periodic_table.cpp.o"
+  "CMakeFiles/libchargefw2.dir/periodic_table.cpp.o.d"
+  "CMakeFiles/libchargefw2.dir/structures/molecule.cpp.o"
+  "CMakeFiles/libchargefw2.dir/structures/molecule.cpp.o.d"
+  "libchargefw2.a"
+  "libchargefw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libchargefw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/CMakeFiles/method.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/src/method.cpp" "src/CMakeFiles/method.dir/method.cpp.o" "gcc" "src/CMakeFiles/method.dir/method.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/structures/CMakeFiles/structures.dir/DependInfo.cmake"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/CMakeFiles/geometry.dir/DependInfo.cmake"
+  "/Libraries/ChargeFW2-7e0916c58eddbb0831de25c0ea11011d2d003059/build/src/utility/CMakeFiles/utility.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
